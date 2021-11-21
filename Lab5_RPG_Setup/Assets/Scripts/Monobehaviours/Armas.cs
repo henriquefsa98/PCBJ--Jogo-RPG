db8264a5fe0f0f7c0b4f88b318156aeaa45d7fd7
Leonardo Vaz <leonardovazlourenco@gmail.com>
2021-11-21 13:51:30 -0300

    lab 9.b - ok
    up

---
+++ Lab5_RPG_Setup/Assets/Scripts/Monobehaviours/Armas.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Armas : MonoBehaviour
+{
+    public GameObject municaoPrefab;                    // armazena o prefab da munição
+    static List <GameObject> municaoPiscina;            // Piscina de munição
+    public int tamanhoPiscina;                          // Tamanho da Piscina
+    public float velocidadeArma;                        // velocidade da Munição
+
+    public void Awake()
+    {
+        if (municaoPiscina == null)
+        {
+            municaoPiscina = new List<GameObject>();
+        }
+        for (int i = 0; i <tamanhoPiscina; i++)
+        {
+            GameObject municaoO = Instantiate(municaoPrefab);
+            municaoO.SetActive(false);
+            municaoPiscina.Add(municaoO);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            DisparaMunicao();
+        }
+    }
+
+    public GameObject SpawnMunicao(Vector3 posicao)
+    {
+        foreach (GameObject municao in municaoPiscina)
+        {
+            if (municao.activeSelf == false)
+            {
+                municao.SetActive(true);
+                municao.transform.position = posicao;
+                return municao;
+            }
+        }
+        return null;
+    }
+
+    void DisparaMunicao()
+    {
+        Vector3 posicaoMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        GameObject municao = SpawnMunicao(transform.position);
+        if (municao != null)
+        {
+            Arco arcoScript = municao.GetComponent<Arco>();
+            float duracaoTrajetoria = 1.0f / velocidadeArma;
+            StartCoroutine(arcoScript.arcoTrajetoria(posicaoMouse, duracaoTrajetoria));              // ******** linha não compila ********
+        }
+    }
+
+    private void OnDestroy()
+    {
+        municaoPiscina = null;
+    }
+
+}
